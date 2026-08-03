47d7cbf3b6098591bbd288763e2cde9ebdf20bd5
chakri3306 <chakri.janyavula789@gmail.com>
2026-08-03 14:12:29 +0530

    Initial commit: Add Exercism Python workspace

---
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -0,0 +1,12 @@
+EXPECTED_BAKE_TIME = 40 
+def bake_time_remaining(elapsed_bake_time):
+    result = EXPECTED_BAKE_TIME - elapsed_bake_time
+def preparation_time_in_minutes(number_of_layers):
+    return number_of_layers*2
+    
+def elapsed_time_in_minutes(number_of_layers,elapsed_bake_time):
+    return number_of_layers*2 + elapsed_bake_time
+    
+    
+    
+    
